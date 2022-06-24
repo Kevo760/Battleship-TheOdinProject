08edccb7f0d62bc2e9abb1e01b25af8d319e7456
TheLegend760 <73809530+TheLegend760@users.noreply.github.com>
2022-06-24 00:55:56 -0700

    gameBoard.js
    - Added more functions
    - Fixed a few functions

--- src/createGame.js
+++ src/createGame.js
@@ -5,10 +5,24 @@ const createGame = () => {
 const ship1 = new Ship('destroyer', 3);
 
 
-console.log(ship1)
+//console.log(ship1)
 
 
 const array = new Gameboard();
+
+array.placeShip(ship1, 0, 0, true)
+
+
+array.receiveAttack(0,0);
+array.receiveAttack(9,9);
+array.receiveAttack(9,9);
+
+
+console.log(array);
+
+
+
+
 
 
 
--- src/gameboardFactory.js
+++ src/gameboardFactory.js
@@ -1,33 +1,105 @@
+import { pick } from "lodash";
 
 
 class Gameboard {
 
     constructor() {
-        this.board = [];
-        this.createGameBoard();
+        this.board;
+        this.missedShots = [];
+        this.shotsMade = [];
+        this.createBoard();
     }
 
-
-    createGameBoard() {
-        const rows = 10;
-        const columns = 10;
-        // Creates row array
-        for(let i = 0; i < rows; i++) {
-            this.board[i] = [];
-            // Creates column array
-            for(let j = 0; j < columns; j++) {
-                // 0-99 array counts
-                this.board[i][j] = (i*columns)+j;
-            }
-        }
+    // Creates the 10x10 board with value of 0
+    createBoard() {
+        return this.board = ([...Array(10)].map( () => Array(10).fill(0)))
     }
 
 
     placeShip(ship, row, column, verticle) {
-        
+        if(!this.possiblePlacement(ship, row, column, verticle)) return false;
+
+        // If verticle is true place ship on designated area based on ship length
+        if(verticle) {
+            for(let i = 0; i < ship.length; i++) {
+                this.board[row + i][column] = ship;
+            }
+        // If verticle is false place ship on designated area based on ship length
+        } else if(!verticle) {
+            for(let i = 0; i < ship.length; i++) {
+                this.board[row][column + i] = ship;
+            }
+        }
+        return true;
     };
 
+
+    possiblePlacement(ship, row, column, verticle) {
+        // check verticle placement is true
+        if(verticle) {
+            for(let i = 0; i < ship.length; i++) {
+                // while row placement is taken or invalid return false
+                while(this.board[row + i][column] !== 0) {
+                    return false
+                }
+            }
+            return true
+        // If verticle is false 
+        } else if(!verticle) {
+            for(let i = 0; i < ship.length; i++) {
+                // while column placement is taken or invalid return false
+                while(this.board[row][column + i] !== 0) {
+                    return false
+                }
+            }
+            return true
+        }
+    }
+
+
+    receiveAttack(row, column) {
+        // Creates a position array with row and column
+        const position = [row, column];
+        
+        // If positionShot is false return true
+        if(this.alreadyShot(position) == false) {
+
+        // If board selection does not equal 0, pass hitShip and isSunk function on ship and push position on shotsMade
+        if(this.board[row][column] !== 0) {
+                this.board[row][column].hitShip(position);
+                this.board[row][column].isSunk();
+                this.shotsMade.push(position);
+            // Else push position array into missedShot and shotsMade array
+            } else {
+                this.missedShots.push(position);
+                this.shotsMade.push(position);
+            }
+
+            return true
+        } else {
+            return false
+        }
+
+
+
+    }
+
+
+    alreadyShot(position) {
+        const myArray = this.shotsMade
+        // true or false staments that finds if the position matches the shots made array
+        return myArray.some(arrays => position.every((e, location) => e === arrays[location]))
+    }
+
+
+    checkAllShipsSunk() {
+
+    }
+
+
 }
+
+
 
 
 
